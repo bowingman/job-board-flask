b870ec4118d63929c0881b40d83c3b2b36360a15
rabbit <rabbit@sky.io>
2022-08-04 07:47:12 -0700

    added: registraion resolver

--- api/models/user.py
+++ api/models/user.py
@@ -5,8 +5,9 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
-    passowrd = db.Column(db.String, nullable=False)
+    password = db.Column(db.String)
     role = db.Column(db.String(20), default="freelancer")
     title = db.Column(db.String, nullable=True)
     description = db.Column(db.String, nullable=True)
     rate = db.Column(db.Integer, default=0)
+    approved = db.Column(db.Boolean, default=False)
--- api/resolvers/auth_resolver.py
+++ api/resolvers/auth_resolver.py
@@ -1,6 +1,48 @@
+from graphql import GraphQLError
+from api import db
+from api.models import User
+from werkzeug.security import generate_password_hash
+from sqlalchemy.exc import IntegrityError
+from graphql import GraphQLError
+
+
 def login_resolver(obj, info, input):
     return {
         'success': False,
         'token': '123',
         'errors': ['erorr']
     }
+
+
+def register_resolver(obj, info, input):
+    clean_input = {
+        'name': input.get('name'),
+        'password': input.get('password'),
+        'role': input.get('role'),
+        'title': input.get('title'),
+        'description': input.get('description'),
+    }
+
+    try:
+        hashed_password = generate_password_hash(clean_input['password'])
+        user = User(
+            name=clean_input['name'],
+            password=hashed_password,
+            role=clean_input['role'],
+            title=clean_input['title'],
+            description=clean_input['description'],
+            rate=0,
+            approved=False,
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return {
+            'success': True,
+            'user': user
+        }
+
+    except IntegrityError as err:
+        errorInfo = err.orig.args
+        raise GraphQLError(message=errorInfo)
