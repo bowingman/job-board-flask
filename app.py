82d3ac93faf5235da6dbfccacfca6abaec984015
rabbit <rabbit@sky.io>
2022-08-04 05:25:14 -0700

    flask migrate init

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 api = Api(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class HelloWorld(Resource):
